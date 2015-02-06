2410393de52273c28054884c61f85f5ed5a4cdd9
reustonium <andrew.ruestow@gmail.com>
2015-02-06 11:00:26 -0500

    removed menu from actionbar

--- app/src/main/java/yellr/net/yellr_android/activities/HomeActivity.java
+++ app/src/main/java/yellr/net/yellr_android/activities/HomeActivity.java
@@ -141,28 +141,6 @@ public class HomeActivity extends ActionBarActivity implements ActionBar.TabList
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
         // When the given tab is selected, switch to the corresponding page in
         // the ViewPager.
