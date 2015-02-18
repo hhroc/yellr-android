1f92522dcb7104882bbf2d8dbe4cf2a3bcde167c
reustonium <andrew.ruestow@gmail.com>
2015-02-18 13:34:01 -0500

    removed extra listener

--- app/src/main/java/yellr/net/yellr_android/activities/NotificationsActivity.java
+++ app/src/main/java/yellr/net/yellr_android/activities/NotificationsActivity.java
@@ -1,13 +1,12 @@
 package yellr.net.yellr_android.activities;
 
-import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
 import yellr.net.yellr_android.R;
 import yellr.net.yellr_android.fragments.NotificationsFragment;
 
-public class NotificationsActivity extends ActionBarActivity implements NotificationsFragment.OnFragmentInteractionListener {
+public class NotificationsActivity extends ActionBarActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,27 +18,4 @@ public class NotificationsActivity extends ActionBarActivity implements Notifica
                     .commit();
         }
     }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    /*
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_notifications, container, false);
-            return rootView;
-        }
-    }
-    */
 }
