e4d650d1564a8c8a4ab2c4209ee892ee3bee4bc5
reustonium <andrew.ruestow@gmail.com>
2015-02-18 08:15:49 -0500

    fixed nav up bug on postactivity

--- app/src/main/java/yellr/net/yellr_android/activities/PostActivity.java
+++ app/src/main/java/yellr/net/yellr_android/activities/PostActivity.java
@@ -1,7 +1,10 @@
 package yellr.net.yellr_android.activities;
 
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import yellr.net.yellr_android.R;
 import yellr.net.yellr_android.fragments.PostFragment;
@@ -11,6 +14,12 @@ public class PostActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Setup the action bar
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         setContentView(R.layout.activity_post);
         if (savedInstanceState == null) {
             int assignmentId = getIntent().getExtras().getInt(PostFragment.ARG_ASSIGNMENT_ID);
@@ -20,5 +29,15 @@ public class PostActivity extends ActionBarActivity {
                     .add(R.id.container, new PostFragment().newInstance(assignmentId, questionText, questionDescription))
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
