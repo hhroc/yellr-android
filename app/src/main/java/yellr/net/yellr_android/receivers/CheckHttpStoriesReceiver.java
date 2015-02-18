7015139a1a796731baaa3f26de3db7a2ffdd2bff
reustonium <andrew.ruestow@gmail.com>
2015-02-18 10:59:02 -0500

    launch notifications with pendingIntents upon a new story or assignment closes #98

--- app/src/main/java/yellr/net/yellr_android/receivers/CheckHttpStoriesReceiver.java
+++ app/src/main/java/yellr/net/yellr_android/receivers/CheckHttpStoriesReceiver.java
@@ -1,14 +1,21 @@
 package yellr.net.yellr_android.receivers;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.google.gson.Gson;
 
 import java.util.Arrays;
 
+import yellr.net.yellr_android.R;
+import yellr.net.yellr_android.activities.ViewStoryActivity;
+import yellr.net.yellr_android.fragments.ViewStoryFragment;
 import yellr.net.yellr_android.intent_services.stories.StoriesIntentService;
 import yellr.net.yellr_android.intent_services.stories.StoriesResponse;
 import yellr.net.yellr_android.utils.YellrUtils;
@@ -47,7 +54,37 @@ public class CheckHttpStoriesReceiver extends BroadcastReceiver {
             }
 
             if (newStories) {
-                // TODO: create android notification that there is a new story
+
+                Intent storyIntent;
+                storyIntent = new Intent(context, ViewStoryActivity.class);
+                storyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                if(response.stories.length >0) {
+                    int sIndex = 0;
+                    NotificationCompat.Builder mBuilder =
+                            new NotificationCompat.Builder(context)
+                                    .setSmallIcon(R.drawable.icon)
+                                    .setContentTitle(response.stories[sIndex].title)
+                                    .setContentText(response.stories[sIndex].contents);
+
+                    storyIntent.putExtra(ViewStoryFragment.ARG_STORY_TITLE, response.stories[sIndex].title);
+                    storyIntent.putExtra(ViewStoryFragment.ARG_STORY_AUTHOR, response.stories[sIndex].author_first_name + " " + response.stories[sIndex].author_last_name);
+                    storyIntent.putExtra(ViewStoryFragment.ARG_STORY_PUBLISHED_DATETIME, response.stories[sIndex].publish_datetime);
+                    storyIntent.putExtra(ViewStoryFragment.ARG_STORY_BANNER_MEDIA_FILE_NAME, response.stories[sIndex].banner_media_file_name);
+                    storyIntent.putExtra(ViewStoryFragment.ARG_STORY_TOP_TEXT, response.stories[sIndex].top_text);
+                    //intent.putExtra(ViewStoryFragment.ARG_STORY_CONTENTS,response.stories[position].contents);
+                    storyIntent.putExtra(ViewStoryFragment.ARG_STORY_CONTENTS_RENDERED, response.stories[sIndex].contents_rendered);
+
+                    PendingIntent pendingStoryIntent = PendingIntent.getActivity(context, 0, storyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                    mBuilder.setContentIntent(pendingStoryIntent);
+                    int storyNotificationId = 1;
+                    NotificationManager mNotificationMgr =
+                            (NotificationManager)  context.getSystemService(Context.NOTIFICATION_SERVICE);
+                    mNotificationMgr.notify(storyNotificationId, mBuilder.build());
+                }
+
+
                 Log.d("CheckHttpStoriesReceiver.onReceive()", "New Stories!");
             }
 
